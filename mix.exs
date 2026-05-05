c77cd902c720fc809921288bd43f73aec3cd6173
Kamil Skowron <1019893+Cinderella-Man@users.noreply.github.com>
2026-05-05 19:45:05 +0200

    Version++

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Credence.MixProject do
   def project do
     [
       app: :credence,
-      version: "0.2.0",
+      version: "0.3.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
