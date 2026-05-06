1ee9a08e7d2084e269f5040acc8cb21b55d8067e
Kamil Skowron <1019893+Cinderella-Man@users.noreply.github.com>
2026-05-06 15:32:50 +0200

    More renaming etc

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Credence.MixProject do
   def project do
     [
       app: :credence,
-      version: "0.3.2",
+      version: "0.4.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
