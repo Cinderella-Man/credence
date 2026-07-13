a7a4b796be74b0d6b7f798130592f1c003338d89
Kamil Skowron <1019893+Cinderella-Man@users.noreply.github.com>
2026-07-13 03:03:40 +0200

    cred-gen: new(semantic): no_bare_function_def_syntax [row 96]

---
+++ test/semantic/no_bare_function_def_syntax_fix_test.exs
@@ -0,0 +1,87 @@
+defmodule Credence.Semantic.NoBareFunctionDefSyntaxFixTest do
+  use ExUnit.Case
+
+  import Credence.RuleCase, only: [confirm_fix: 2, valid_syntax?: 1]
+
+  alias Credence.Semantic.NoBareFunctionDefSyntax
+
+  @bare_init ~S'''
+  defmodule Foo do
+    use Plug.Router
+
+    plug :match
+    plug :dispatch
+
+    init(opts) do
+      opts
+    end
+
+    get "/ping" do
+      send_resp(conn, 200, "pong")
+    end
+  end
+  '''
+
+  @fixed_init ~S'''
+  defmodule Foo do
+    use Plug.Router
+
+    plug :match
+    plug :dispatch
+
+    def init(opts) do
+      opts
+    end
+
+    get "/ping" do
+      send_resp(conn, 200, "pong")
+    end
+  end
+  '''
+
+  defp fix(source, message, line) do
+    NoBareFunctionDefSyntax.fix(source, %{
+      severity: :error,
+      message: message,
+      position: {line, 1}
+    })
+  end
+
+  test "fixes bare init in Plug.Router" do
+    message = "undefined function init/2 (there is no such import)"
+    confirm_fix(fix(@bare_init, message, 7), @fixed_init)
+  end
+
+  test "fixed output is well-formed (parses)" do
+    message = "undefined function init/2 (there is no such import)"
+    assert valid_syntax?(fix(@bare_init, message, 7))
+  end
+
+  test "ignores lines that already have def" do
+    already_def = ~S'''
+    defmodule Foo do
+      def init(opts) do
+        opts
+      end
+    end
+    '''
+
+    message = "undefined function init/2 (there is no such import)"
+    # Line 2 already has `def`, so the bare regex won't match
+    confirm_fix(fix(already_def, message, 2), already_def)
+  end
+
+  test "ignores lines without do block" do
+    call_only = ~S'''
+    defmodule Foo do
+      def bar do
+        init(opts)
+      end
+    end
+    '''
+
+    message = "undefined function init/2 (there is no such import)"
+    # Line 3 is a call without `do`, not a bare function def
+    confirm_fix(fix(call_only, message, 3), call_only)
+  end
+end
