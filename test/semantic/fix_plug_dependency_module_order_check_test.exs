0190ff5f0ecf46eca797a048e8b95e7d6941736d
Kamil Skowron <1019893+Cinderella-Man@users.noreply.github.com>
2026-07-23 10:19:42 +0200

    fix_plug_dependency_module_order: accepted

---
+++ test/semantic/fix_plug_dependency_module_order_check_test.exs
@@ -0,0 +1,106 @@
+defmodule Credence.Semantic.FixPlugDependencyModuleOrderCheckTest do
+  use ExUnit.Case
+
+  alias Credence.Semantic.FixPlugDependencyModuleOrder
+
+  @matching_diag %{
+    severity: :error,
+    message:
+      "function MediaVersionApi.Plugs.AcceptVersion.init/1 is undefined (module MediaVersionApi.Plugs.AcceptVersion is not available)",
+    position: 0,
+    file: "credence_check.ex"
+  }
+
+  test "matches the diagnostic" do
+    assert FixPlugDependencyModuleOrder.match?(@matching_diag)
+  end
+
+  test "matches the real compiler message including the trailing hint" do
+    diag = %{
+      severity: :error,
+      message:
+        "function Foo.Plugs.Bar.init/1 is undefined (module Foo.Plugs.Bar is not available). " <>
+          "Make sure the module name is correct and has been specified in full (or that an alias has been defined)",
+      position: 0
+    }
+
+    assert FixPlugDependencyModuleOrder.match?(diag)
+  end
+
+  test "ignores unrelated diagnostics" do
+    diag = %{severity: :warning, message: "unrelated", position: {1, 1}}
+    refute FixPlugDependencyModuleOrder.match?(diag)
+  end
+
+  test "ignores generic undefined function without module-not-available" do
+    diag = %{severity: :error, message: "undefined function foo/1", position: 1}
+    refute FixPlugDependencyModuleOrder.match?(diag)
+  end
+
+  test "ignores undefined functions other than init/1" do
+    diag = %{
+      severity: :error,
+      message: "function Foo.bar/2 is undefined (module Foo is not available)",
+      position: 0
+    }
+
+    refute FixPlugDependencyModuleOrder.match?(diag)
+  end
+
+  test "ignores the warning-severity yet-to-be-defined form" do
+    diag = %{
+      severity: :warning,
+      message:
+        "Foo.init/1 is undefined (module Foo is not available or is yet to be defined)",
+      position: {1, 1}
+    }
+
+    refute FixPlugDependencyModuleOrder.match?(diag)
+  end
+
+  test "ignores messages where the unavailable module differs from the called one" do
+    diag = %{
+      severity: :error,
+      message: "function Foo.init/1 is undefined (module Bar is not available)",
+      position: 0
+    }
+
+    refute FixPlugDependencyModuleOrder.match?(diag)
+  end
+
+  test "attributes the issue to this rule" do
+    assert FixPlugDependencyModuleOrder.to_issue(@matching_diag).rule ==
+             :fix_plug_dependency_module_order
+  end
+
+  test "should_report? is true only when the fix would reorder the source" do
+    fixable = ~S"""
+    defmodule MediaVersionApi.Router do
+      use Plug.Router
+
+      plug(MediaVersionApi.Plugs.AcceptVersion)
+      plug(:match)
+      plug(:dispatch)
+    end
+
+    defmodule MediaVersionApi.Plugs.AcceptVersion do
+      def init(opts), do: opts
+      def call(conn, _opts), do: conn
+    end
+    """
+
+    assert FixPlugDependencyModuleOrder.should_report?(@matching_diag, fixable)
+
+    no_dep_module = ~S"""
+    defmodule MediaVersionApi.Router do
+      use Plug.Router
+
+      plug(MediaVersionApi.Plugs.AcceptVersion)
+      plug(:match)
+      plug(:dispatch)
+    end
+    """
+
+    refute FixPlugDependencyModuleOrder.should_report?(@matching_diag, no_dep_module)
+  end
+end
---
+++ test/semantic/fix_plug_dependency_module_order_fix_test.exs
@@ -0,0 +1,271 @@
+defmodule Credence.Semantic.FixPlugDependencyModuleOrderFixTest do
+  use ExUnit.Case
+
+  import Credence.RuleCase, only: [confirm_fix: 2, valid_syntax?: 1]
+
+  alias Credence.Semantic.FixPlugDependencyModuleOrder
+
+  @diag_message "function MediaVersionApi.Plugs.AcceptVersion.init/1 is undefined (module MediaVersionApi.Plugs.AcceptVersion is not available)"
+
+  defp fix(source, message \\ @diag_message, line \\ 0) do
+    FixPlugDependencyModuleOrder.fix(source, %{
+      severity: :error,
+      message: message,
+      position: line
+    })
+  end
+
+  test "reorders modules so dependency comes first" do
+    input = ~S"""
+    defmodule MediaVersionApi.Router do
+      use Plug.Router
+      import Plug.Conn
+
+      plug(MediaVersionApi.Plugs.AcceptVersion)
+      plug(:match)
+      plug(:dispatch)
+
+      get "/hello" do
+        send_resp(conn, 200, "world")
+      end
+    end
+
+    defmodule MediaVersionApi.Plugs.AcceptVersion do
+      @behaviour Plug
+
+      @impl true
+      def init(opts), do: opts
+
+      @impl true
+      def call(conn, _opts), do: conn
+    end
+    """
+
+    expected = ~S"""
+    defmodule MediaVersionApi.Plugs.AcceptVersion do
+      @behaviour Plug
+
+      @impl true
+      def init(opts), do: opts
+
+      @impl true
+      def call(conn, _opts), do: conn
+    end
+
+    defmodule MediaVersionApi.Router do
+      use Plug.Router
+      import Plug.Conn
+
+      plug(MediaVersionApi.Plugs.AcceptVersion)
+      plug(:match)
+      plug(:dispatch)
+
+      get "/hello" do
+        send_resp(conn, 200, "world")
+      end
+    end
+    """
+
+    confirm_fix(fix(input), expected)
+  end
+
+  test "fixed output is well-formed (parses)" do
+    input = ~S"""
+    defmodule Foo.Router do
+      use Plug.Router
+
+      plug(Foo.Plugs.Bar)
+      plug(:match)
+      plug(:dispatch)
+
+      match _ do
+        send_resp(conn, 200, "ok")
+      end
+    end
+
+    defmodule Foo.Plugs.Bar do
+      def init(opts), do: opts
+      def call(conn, _opts), do: conn
+    end
+    """
+
+    message = "function Foo.Plugs.Bar.init/1 is undefined (module Foo.Plugs.Bar is not available)"
+    assert valid_syntax?(fix(input, message))
+  end
+
+  test "returns source unchanged when dependency already comes first" do
+    input = ~S"""
+    defmodule MediaVersionApi.Plugs.AcceptVersion do
+      @behaviour Plug
+
+      @impl true
+      def init(opts), do: opts
+
+      @impl true
+      def call(conn, _opts), do: conn
+    end
+
+    defmodule MediaVersionApi.Router do
+      use Plug.Router
+      import Plug.Conn
+
+      plug(MediaVersionApi.Plugs.AcceptVersion)
+      plug(:match)
+      plug(:dispatch)
+    end
+    """
+
+    confirm_fix(fix(input), input)
+  end
+
+  test "returns source unchanged when message does not match" do
+    input = """
+    defmodule A do
+    end
+
+    defmodule B do
+    end
+    """
+
+    confirm_fix(fix(input, "some unrelated error"), input)
+  end
+
+  test "reorders correctly when the router contains a nested helper module before the plug call" do
+    input = ~S"""
+    defmodule Foo.Router do
+      defmodule Helper do
+        def status, do: :ok
+      end
+
+      use Plug.Router
+
+      plug(Foo.Plugs.Bar)
+      plug(:match)
+      plug(:dispatch)
+    end
+
+    defmodule Foo.Plugs.Bar do
+      def init(opts), do: opts
+      def call(conn, _opts), do: conn
+    end
+    """
+
+    expected = ~S"""
+    defmodule Foo.Plugs.Bar do
+      def init(opts), do: opts
+      def call(conn, _opts), do: conn
+    end
+
+    defmodule Foo.Router do
+      defmodule Helper do
+        def status, do: :ok
+      end
+
+      use Plug.Router
+
+      plug(Foo.Plugs.Bar)
+      plug(:match)
+      plug(:dispatch)
+    end
+    """
+
+    message = "function Foo.Plugs.Bar.init/1 is undefined (module Foo.Plugs.Bar is not available)"
+    confirm_fix(fix(input, message), expected)
+  end
+
+  test "returns source unchanged when the dependency is nested inside the router" do
+    input = ~S"""
+    defmodule Foo.Router do
+      use Plug.Router
+
+      plug(Foo.Plugs.Bar)
+      plug(:match)
+      plug(:dispatch)
+
+      defmodule Foo.Plugs.Bar do
+        def init(opts), do: opts
+        def call(conn, _opts), do: conn
+      end
+    end
+    """
+
+    message = "function Foo.Plugs.Bar.init/1 is undefined (module Foo.Plugs.Bar is not available)"
+    confirm_fix(fix(input, message), input)
+  end
+
+  test "returns source unchanged when the dependency module is not defined in the file" do
+    input = ~S"""
+    defmodule Foo.Router do
+      use Plug.Router
+
+      plug(Foo.Plugs.Bar)
+      plug(:match)
+      plug(:dispatch)
+    end
+    """
+
+    message = "function Foo.Plugs.Bar.init/1 is undefined (module Foo.Plugs.Bar is not available)"
+    confirm_fix(fix(input, message), input)
+  end
+
+  test "reorders modules when plug uses short alias after alias declaration" do
+    input = ~S"""
+    defmodule LifecycleApi.Router do
+      use Plug.Router
+
+      import Plug.Conn
+
+      alias LifecycleApi.Plugs.ApiVersion
+
+      plug :match
+      plug :fetch_query_params
+      plug :fetch_headers
+      plug(ApiVersion, default: "v2")
+      plug :dispatch
+
+      get "/api/users/:id" do
+        conn |> send_resp(200, "ok")
+      end
+    end
+
+    defmodule LifecycleApi.Plugs.ApiVersion do
+      @moduledoc false
+      def init(opts), do: opts
+
+      def call(conn, _opts), do: conn
+    end
+    """
+
+    expected = ~S"""
+    defmodule LifecycleApi.Plugs.ApiVersion do
+      @moduledoc false
+      def init(opts), do: opts
+
+      def call(conn, _opts), do: conn
+    end
+
+    defmodule LifecycleApi.Router do
+      use Plug.Router
+
+      import Plug.Conn
+
+      alias LifecycleApi.Plugs.ApiVersion
+
+      plug :match
+      plug :fetch_query_params
+      plug :fetch_headers
+      plug(ApiVersion, default: "v2")
+      plug :dispatch
+
+      get "/api/users/:id" do
+        conn |> send_resp(200, "ok")
+      end
+    end
+    """
+
+    message =
+      "function LifecycleApi.Plugs.ApiVersion.init/1 is undefined (module LifecycleApi.Plugs.ApiVersion is not available)"
+
+    confirm_fix(fix(input, message), expected)
+  end
+end
